99823622cc1a654aaa445c85f50460bc4ca91bab
Chris Webster <cjw@ucar.edu>
2025-02-08 08:16:26 -0700

    Switch to passed in directory to build from.
    Default for Jenkins is /root and we want to build from /root/current

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-pwd
+# Jenkins lands in /root, so allow us to pass in a directory to build from.
+if [ -n "$1" ]; then
+  cd $1
+fi
 
 for dir in ads-daq ads-daq2 eol-daq eol-repo
 do
